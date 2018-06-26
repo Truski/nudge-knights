2ef598d914f6f7113dd297460e7cc74809a46c26
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-06-26 01:33:01 -0500

    Change game to Game + Screen

--- core/src/io/charstar/nudgeknights/NudgeKnights.java
+++ core/src/io/charstar/nudgeknights/NudgeKnights.java
@@ -1,33 +1,69 @@
 package io.charstar.nudgeknights;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class NudgeKnights extends ApplicationAdapter {
+public class NudgeKnights extends Game implements Screen {
   SpriteBatch batch;
-  Texture img;
+  Knight knight;
 
   @Override
   public void create () {
     batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+    knight = new Knight(0, 0);
+    setScreen(this);
   }
 
   @Override
-  public void render () {
+  public void show() {
+
+  }
+
+  @Override
+  public void hide() {
+
+  }
+
+  @Override
+  public void resize(int width, int height) {
+
+  }
+
+  @Override
+  public void pause() {
+
+  }
+
+  @Override
+  public void resume() {
+
+  }
+
+  @Override
+  public void render(float delta) {
+
+    // Process events
+    if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+      knight.jump();
+    }
+
+    // Update Game
+    knight.update(delta);
+
+    // Render Game
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     batch.begin();
-    batch.draw(img, 0, 0);
+
+    knight.draw(batch);
+
     batch.end();
   }
 
   @Override
   public void dispose () {
     batch.dispose();
-    img.dispose();
   }
 }
