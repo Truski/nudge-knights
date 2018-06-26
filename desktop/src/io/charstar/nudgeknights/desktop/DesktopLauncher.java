6425e9aa693e36bd441caa763dac887222b3ac11
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-06-26 00:45:43 -0500

    Set window initial width and height to 1280x720

--- desktop/src/io/charstar/nudgeknights/desktop/DesktopLauncher.java
+++ desktop/src/io/charstar/nudgeknights/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import io.charstar.nudgeknights.NudgeKnights;
 public class DesktopLauncher {
   public static void main (String[] arg) {
     LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.width = 1280;
+    config.height = 720;
     new LwjglApplication(new NudgeKnights(), config);
   }
 }
