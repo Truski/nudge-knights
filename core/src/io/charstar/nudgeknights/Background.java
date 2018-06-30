2fb81847531db23f7838dad67322ace2b9dd5c23
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-06-30 01:42:47 -0500

    Redefine floor as bottom of bounding box

---
+++ core/src/io/charstar/nudgeknights/Background.java
@@ -0,0 +1,4 @@
+package io.charstar.nudgeknights;
+
+public class Background {
+}
--- core/src/io/charstar/nudgeknights/Knight.java
+++ core/src/io/charstar/nudgeknights/Knight.java
@@ -10,10 +10,11 @@ import com.badlogic.gdx.math.Vector2;
 public class Knight {
   private static final int WIDTH = 42;
   private static final int HEIGHT = 42;
-  private static final int SPEED = 10;
-  private static final float GRAVITY = -9.8f;
+  private static final int SPEED = 50;
+  private static final float GRAVITY = -20f;
   private static final int ANIMATION_FRAMERATE = 15;
   private static final int ANIMATION_FRAMES = 4;
+  private static final int FLOOR = 0;
 
   private static TextureRegion[] idle = TextureRegion.split(
       new Texture("idle.png"), WIDTH, HEIGHT)[0];
@@ -29,10 +30,10 @@ public class Knight {
   private byte turnDirection;
 
   public Knight(float x, float y) {
-    position = new Vector2(x, y);
+    position = new Vector2(WIDTH / 2, HEIGHT / 2);
     velocity = new Vector2();
     acceleration = new Vector2();
-    box = new Rectangle(-WIDTH / 2, -HEIGHT / 2, WIDTH, HEIGHT);
+    box = new Rectangle(0, 0, WIDTH, HEIGHT);
 
     inAir = false;
     turnDirection = 1;
@@ -44,15 +45,15 @@ public class Knight {
   }
 
   public void draw(ShapeRenderer renderer){
-    renderer.rect(box.x - box.width / 2, box.y - box.height / 2, box.width, box.height);
+    renderer.rect(box.x, box.y, box.width, box.height);
   }
 
   public void update(float delta){
     animation += delta * ANIMATION_FRAMERATE;
     if(animation >= ANIMATION_FRAMES) animation = 0;
 
-    if(position.y < 0){
-      position.y = 0;
+    if(box.y < 0){
+      position.y = HEIGHT / 2;
       velocity.y = 0;
       inAir = false;
     }
@@ -63,8 +64,8 @@ public class Knight {
     Vector2 velocity = new Vector2(this.velocity).scl(delta);
     position.add(velocity);
 
-    box.x = position.x;
-    box.y = position.y;
+    box.x = position.x - WIDTH / 2;
+    box.y = position.y - HEIGHT / 2;
   }
 
   public void jump(){
