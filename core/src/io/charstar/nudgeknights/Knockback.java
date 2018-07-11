cc756ff95db3ba7f9eace516cb5fbd4d51e410a6
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-07-10 22:55:11 -0500

    Implement knockback class

--- core/src/io/charstar/nudgeknights/Knockback.java
+++ core/src/io/charstar/nudgeknights/Knockback.java
@@ -4,4 +4,30 @@ public class Knockback {
   private float velocity;
   private float timeRemaining;
   private boolean active;
+
+  public void set(float velocity, float time){
+    this.velocity = velocity;
+    this.timeRemaining = time;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void update(float delta){
+    if(active){
+      timeRemaining -= delta;
+      if(timeRemaining <= 0){
+        active = false;
+      }
+    }
+  }
+
+  public void apply(Knight knight){
+    knight.getVelocity().x = velocity;
+  }
 }
